f79b5362314efe465b578843a4497abcca8caede
Dishant Sheth <dishantnirov.sheth2015@vit.ac.in>
2018-03-29 16:43:19 +0530

    Bug fixes

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/iot/images', express.static(path.join(__dirname, 'user_images')));
 
 app.use('/iot/', index);
 app.use('/iot/users', users);
