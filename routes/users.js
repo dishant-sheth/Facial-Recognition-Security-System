97dbb40b0f2ce15b0a14aa7d71334bfb06131bc2
Dishant Sheth <dishantnirov.sheth2015@vit.ac.in>
2018-03-29 22:45:49 +0530

    Fixes

--- routes/users.js
+++ routes/users.js
@@ -39,10 +39,15 @@ router.post('/recognize-user', multer({ storage: search_storage}).single('upload
   const image_url = 'http://vu.adgvit.com/iot/search_images/' + req.body.filename;
   faceRecogSvc.recognizeUser(image_url)
     .then((result) => {
-      const face_id = result.transaction.face_id;
-      const subject_id = result.transaction.subject_id;
-      console.log(face_id + " - " + subject_id);
-      return userSvc.getUsers({ name: subject_id });
+      if(result.transaction.status === 'failure'){
+        res.status(400).send({ message: 'The user doesn\'t exist in the userbase.'});
+      }
+      else {
+        const face_id = result.transaction.face_id;
+        const subject_id = result.transaction.subject_id;
+        console.log(face_id + " - " + subject_id);
+        return userSvc.getUsers({ name: subject_id });
+      }
     })
     .then((result) => {
       res.status(200).send(result);
